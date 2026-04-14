9b5f021aae0e4d644c6f8e624451ddf99a5af7a2
saniashinde <saniashinde04@gmail.com>
2026-04-14 22:30:13 +0530

    Middlewares

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+const ExpressError = require("./utils/expressError.js");
+const { listingSchema } = require("./schema.js");
+const { reviewSchema } = require("./schema.js");
+
 module.exports.isLoggedIn = (req, res, next) => {
     console.log("USER:", req.user);
     console.log("AUTH:", req.isAuthenticated());
@@ -17,4 +21,26 @@ module.exports.saveRedirectUrl=(req,res,next)=>{
         res.locals.redirectUrl=req.session.redirectUrl;
     }
     next();
+};
+
+module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    else {
+        next();
+    }
+};
+
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    else {
+        next();
+    }
 };
--- routes/listing.js
+++ routes/listing.js
@@ -4,19 +4,8 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { listingSchema } = require("../schema.js");
 const ExpressError = require("../utils/expressError.js");
 const Listing = require("../models/listing.js");
-const {isLoggedIn} = require("../middleware.js");
+const {isLoggedIn,validateListing} = require("../middleware.js");
 
-//middleware
-const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }
-    else {
-        next();
-    }
-}
 
 
 const normalizeMoodTags = (req, res, next) => {
